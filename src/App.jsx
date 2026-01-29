36287061692f0eb74040317ea4e63bdf536124c2
Nippold Annick <Annick.Nippold@gerstaecker.com>
2026-01-29 14:23:31 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import { WeatherIcon } from './components/WeatherIcon'
+
+export const App = () => {
+
+  return (
+      < >
+        <h1>Weather App</h1>
+        <WeatherIcon></WeatherIcon>
+      </>  
+  )
+}
---
+++ src/components/WeatherIcon.jsx
@@ -0,0 +1,11 @@
+import './WeatherIcon.css'
+
+export const WeatherIcon = () => {
+
+  return (
+      < >
+        <img src="\sunny.png" alt="sonnig" />
+      <h2>It´s sunny!</h2>
+      </>
+  )
+}
