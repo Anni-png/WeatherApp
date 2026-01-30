4e6e1b3d85a1d7f6ce7a54121bbde54e8a6e709b
Nippold Annick <Annick.Nippold@gerstaecker.com>
2026-01-30 09:39:00 +0100

    update weather logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import './App.css'
 import { WeatherIcon } from './components/WeatherIcon'
 import { cloudyWeather, sunnyWeather } from './utils/data'
 
-
 export const App = () => {
+
+  let sunny = false;
+
   return (
       < >   
-        <h1>Weather App</h1>
-        <WeatherIcon 
-        weather={sunnyWeather}
-        weather={cloudyWeather}
-        />
+        <h1>Weather App</h1> 
+
+        <WeatherIcon weather={
+        sunny
+           ? sunnyWeather
+            : cloudyWeather
+          } />
       </>  
   )
 }
